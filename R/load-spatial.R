0fe4bcddf638b652d9faadb29e2d4609061e4d54
jarvisc1 <christopher1.jarvis@gmail.com>
2016-09-21 08:50:22 +0100

    Extract raster values for validation point data and save

--- R/load-spatial.R
+++ R/load-spatial.R
@@ -14,18 +14,17 @@ points(p)
 head(p)
 saveRDS(p, "dat/p.Rds")
 
+# load validation data
+pv = SpatialPointsDataFrame(cbind(validate$X, validate$Y), data = validate)
+points(pv)
+head(pv)
+saveRDS(pv, "dat/pv.Rds")
 
-# Turn d into a raster brick for manipulation
-b <- brick(d)
 
-# Get coordinate data for training points
-xy <- data.frame(x =p$X, y = p$Y) 
 
 # Extract raster variables for testing points
-# test raster points
+b <- brick(d)
 trp1 <- raster::extract(b,  p)
-
-# Format as dataframe and save as spatial points
 rp <- as.data.frame(trp1)
 rp$LNDCOV6_100m <- base::factor(as.factor(rp$LNDCOV6_100m), 
                                  levels = 1:28,
@@ -35,9 +34,23 @@ rp$PMTGSS7_100m <- base::factor(as.factor(rp$PMTGSS7_100m),
                                  levels = 1:70,
                                  labels = levels(d$PMTGSS7_100m))
 
-## save a spatial points with raster info for   
 p_rp <- SpatialPointsDataFrame(p, data = rp)
 
 saveRDS(p_rp, "dat/train_rp.RDS")
 
-# repeat process for testing points
+# Extract raster variables for validation points
+vrp1 <- raster::extract(b,  pv)
+vrp <- as.data.frame(vrp1)
+vrp$LNDCOV6_100m <- base::factor(as.factor(vrp$LNDCOV6_100m), 
+                                levels = 1:28,
+                                labels = levels(d$LNDCOV6_100m))
+
+vrp$PMTGSS7_100m <- base::factor(as.factor(vrp$PMTGSS7_100m), 
+                                levels = 1:70,
+                                labels = levels(d$PMTGSS7_100m))
+
+p_vrp <- SpatialPointsDataFrame(pv, data = vrp)
+
+head(p_vrp)
+saveRDS(p_vrp, "dat/validate_rp.RDS")
+
