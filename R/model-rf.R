9f0506dbbfbd3a3c0df11d85ecf8ed0dd45cd6e4
jarvisc1 <christopher1.jarvis@gmail.com>
2016-09-23 09:25:28 +0100

    Add submitted model

---
+++ R/model-rf.R
@@ -0,0 +1,55 @@
+# Aim: Create Submitted MOdel
+
+
+rm(list=ls())
+load('dat/ML.rdata')
+
+# Remove outcome which occur less than 10 times
+tbp_df$TAXNUSDA <- as.character(learn$TAXNUSDA)
+tbp_df <- tbp_df[tbp_df$TAXNUSDA %in% names(table(tbp_df$TAXNUSDA))[table(tbp_df$TAXNUSDA) >= 5],]
+tbp_pca$TAXNUSDA <- as.character(learn$TAXNUSDA)
+tbp_pca <- tbp_pca[tbp_pca$TAXNUSDA %in% names(table(tbp_pca$TAXNUSDA))[table(tbp_pca$TAXNUSDA) >= 5],]
+
+
+# Create dataframe for model
+
+
+pca_l <- data.frame(TAXNUSDA = as.character(tbp_df$TAXNUSDA), Comp.1 = tbp_pca$Comp.1,
+                    Comp.2 = tbp_pca$Comp.2,
+                    Comp.3 = tbp_pca$Comp.3,
+                    Comp.4 = tbp_pca$Comp.4,
+                    slope = tbp_df$DEMNED6_100m_slope,
+                    vdepth = tbp_df$DEMNED6_100m_vdepth,
+                    ex2mod = tbp_df$EX2MOD5_100m,
+                    pri = tbp_df$PR2PRI5_100m,
+                    lnd_c = tbp_df$LNDCOV6_100m,
+                    openp =tbp_df$DEMNED6_100m_openp,
+                    devmean = tbp_df$DEMNED6_100m_devmean,
+                    stringsAsFactors = F)
+
+
+# Random Forest 
+ctrl <- trainControl(method = "repeatedcv", 
+                     number = 10,
+                     repeats = 5)
+mod <- train(TAXNUSDA~., data = pca_l, method = "rf", trControl = ctrl)
+
+print(mod)
+# Create Prediction dataframe
+vpca <- data.frame(Comp.1 = vbp_pca$Comp.1,
+                    Comp.2 = vbp_pca$Comp.2,
+                    Comp.3 = vbp_pca$Comp.3,
+                    Comp.4 = vbp_pca$Comp.4,
+                    slope = vbp_df$DEMNED6_100m_slope,
+                    vdepth = vbp_df$DEMNED6_100m_vdepth,
+                    ex2mod = vbp_df$EX2MOD5_100m,
+                    pri = vbp_df$PR2PRI5_100m,
+                    lnd_c = vbp_df$LNDCOV6_100m,
+                    openp =vbp_df$DEMNED6_100m_openp,
+                    devmean = vbp_df$DEMNED6_100m_devmean,
+                    stringsAsFactors = F)
+
+# Predict outcome
+predrf <- predict(mod, newdata = vpca)
+
+# Export as CSV
